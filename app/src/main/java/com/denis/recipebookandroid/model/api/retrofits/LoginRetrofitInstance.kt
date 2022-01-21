3c0923444932605867b831db936abc74751c51b5
Denis <Denis.sor87@gmail.com>
2022-01-21 12:16:59 +0200

    Registration Made v2

--- app/src/main/java/com/denis/recipebookandroid/model/api/retrofits/LoginRetrofitInstance.kt
+++ app/src/main/java/com/denis/recipebookandroid/model/api/retrofits/LoginRetrofitInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object LoginRetrofitInstance {
 
     private val loginRetrofit: Retrofit
-    private const val BASE_URL = "http://192.168.1.135:8080"
+    private const val BASE_URL = "http://10.0.2.2:8080"
     init {
         val gson: Gson = GsonBuilder().setLenient().create()
 
